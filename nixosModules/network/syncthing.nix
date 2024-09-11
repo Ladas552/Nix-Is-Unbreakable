691dd772a0c208d466e02055a58f82a16d384866
Ladas552 <l.tokshalov@gmail.com>
2024-09-11 19:37:55 +0500

    Waggabind

--- nixosModules/network/syncthing.nix
+++ nixosModules/network/syncthing.nix
@@ -14,46 +14,48 @@
         options.urAccepted = -1;
         devices = {
           "RMX3081" = {
+            id = "$(cat ${config.sops.secrets."mystuff/syncthing/devices/phone".path})";
           };
           "Tab 12" = {
             id = "cat ${config.sops.secrets."mystuff/syncthing/devices/tablet".path}";
           };
         };
-        folders = {
-          "Norg" = {
-            versioning = {
-              type = "trashcan";
-              params.cleanoutDays = "7";
-            };
-            path = "~/Documents/Norg";
-            id = "cat ${config.sops.secrets."mystuff/syncthing/folders/Norg".path}";
-            devices = [
-              "Tab 12"
-              "RMX3081"
-            ];
-          };
-          "Share" = {
-            path = "~/Share";
-            id = "cat ${config.sops.secrets."mystuff/syncthing/folders/Share".path}";
-            devices = [
-              "Tab 12"
-              "RMX3081"
-            ];
-          };
-          "keepass" = {
-            versioning = {
-              type = "trashcan";
-              params.cleanoutDays = "7";
-            };
-            path = "~/Documents/Keepass";
-            id = "cat ${config.sops.secrets."mystuff/syncthing/folders/keepass".path}";
-            devices = [
-              "Tab 12"
-              "RMX3081"
-            ];
-          };
-        };
       };
+      #   folders = {
+      #     "Norg" = {
+      #       versioning = {
+      #         type = "trashcan";
+      #         params.cleanoutDays = "7";
+      #       };
+      #       path = "~/Documents/Norg";
+      #       id = "cat ${config.sops.secrets."mystuff/syncthing/folders/Norg".path}";
+      #       devices = [
+      #         "Tab 12"
+      #         "RMX3081"
+      #       ];
+      #     };
+      #     "Share" = {
+      #       path = "~/Share";
+      #       id = "cat ${config.sops.secrets."mystuff/syncthing/folders/Share".path}";
+      #       devices = [
+      #         "Tab 12"
+      #         "RMX3081"
+      #       ];
+      #     };
+      #     "keepass" = {
+      #       versioning = {
+      #         type = "trashcan";
+      #         params.cleanoutDays = "7";
+      #       };
+      #       path = "~/Documents/Keepass";
+      #       id = "cat ${config.sops.secrets."mystuff/syncthing/folders/keepass".path}";
+      #       devices = [
+      #         "Tab 12"
+      #         "RMX3081"
+      #       ];
+      #     };
+      #   };
+      # };
     };
   };
 }
