5a3edefd128d5ad289202f2b84ecf13d9bb35e92
Ladas552 <l.tokshalov@gmail.com>
2024-11-04 19:11:19 +0500

    Termux integration and theme

--- hosts/NixMux/default.nix
+++ hosts/NixMux/default.nix
@@ -1,4 +1,9 @@
-{ inputs, pkgs, ... }:
+{
+  inputs,
+  pkgs,
+  lib,
+  ...
+}:
 
 {
   # Thanks rix101 for the snippet
@@ -21,9 +26,6 @@
       warn-dirty = false
   '';
 
-  # imports = [
-  #  inputs.home-manager.nixosModules.default
-  # ];
   # Backup etc files instead of failing to activate generation if a file already exists in /etc
   environment.etcBackupExtension = ".bak";
 
@@ -35,6 +37,62 @@
   system.stateVersion = "24.05"; # Set up nix for flakes
   # Set your time zone
   time.timeZone = "Asia/Almaty";
+  # termux settings
+  android-integration = {
+    termux-open.enable = true;
+    termux-open-url.enable = true;
+    termux-reload-settings.enable = true;
+    termux-setup-storage.enable = true;
+  };
+  # for the funny
+  environment.binSh = lib.getExe pkgs.dash;
+  environment.motd = ''
+    Welcome to nix-on-droid!
+
+    * Rebuild system: yy
+    * Ranger TUI: mc
+  '';
+
+  # To not build stuff but download
+  nix.substituters = [
+    "https://devenv.cachix.org"
+    "https://helix.cachix.org"
+  ];
+  nix.trustedPublicKeys = [
+    "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+    "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
+  ];
+
+  # Theme
+  terminal = {
+    font =
+      let
+        jetbrains = pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; };
+        fontPath = "share/fonts/truetype/NerdFonts/JetBrainsMonoNerdFontMono-Regular.ttf";
+      in
+      "${jetbrains}/${fontPath}";
+    colors = {
+      cursor = "#F4DBD6";
+      background = "#181B28";
+      foreground = "#F8F8F2";
+      color0 = "#000000"; # black
+      color1 = "#ff5555"; # red
+      color2 = "#50fa7b"; # green
+      color3 = "#da00e9"; # highligh
+      color4 = "#bd92f8"; # blue
+      color5 = "#ff78c5"; # magenta
+      color6 = "#8ae9fc"; # cyan
+      color7 = "#bbbbbb"; # white
+      color8 = "#545454"; # br black
+      color9 = "#ff5454"; # br red
+      color10 = "#50fa7b"; # br green
+      color11 = "#f0fa8b"; # br yellow
+      color12 = "#bd92f8"; # br blue
+      color13 = "#ff78c5"; # br magenta
+      color14 = "#8ae9fc"; # br cyan
+      color15 = "#ffffff"; # br white
+    };
+  };
 
   home-manager = {
     extraSpecialArgs = {
