97f4569c104a6955e80c9f6794566800697945d0
Ladas552 <l.tokshalov@gmail.com>
2025-01-16 11:08:53 +0500

    update wsl

--- flake.nix
+++ flake.nix
@@ -89,6 +89,7 @@
         };
         # NixOS WSL setup
         NixwsL = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
           specialArgs = {
             inherit inputs pkgs-stable;
           };
