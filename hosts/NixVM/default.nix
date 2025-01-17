c4d1208d839d994e28d5f66cfe3dadedc0037872
Ladas552 <l.tokshalov@gmail.com>
2025-01-17 14:48:01 +0500

    cool

--- hosts/NixVM/default.nix
+++ hosts/NixVM/default.nix
@@ -212,7 +212,7 @@
     trim.enable = true;
   };
   # standardized filesystem layo
-  fileSystems = {
+  fileSystems = lib.default {
     "/" = {
       device = "zroot/root";
       fsType = "zfs";
