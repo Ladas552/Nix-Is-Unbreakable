80eb233e27d8af9e5bb63c93b3d2ee3603cf81a0
Ladas552 <l.tokshalov@gmail.com>
2025-03-02 15:57:31 +0500

    lf zoxide and some preview styff with kitty

--- homeModules/lf/options/commands.nix
+++ homeModules/lf/options/commands.nix
@@ -1,20 +1,27 @@
 { pkgs, lib, ... }:
 {
-# Todo
-# edit "open" command to pistol as tutorial
+  # Todo
+  # edit "open" command to pistol as tutorial
   programs.lf = {
     commands = {
       trash = ''%${lib.getExe' pkgs.trash-cli "trash-put"} "$fx"'';
       open-nvim = ''$nvim $f'';
       open-hx = ''$hx $f'';
-      q = "quit";
-      # mkdir = ''
-      #   ''${{
-      #     printf "Directory Name: "
-      #     read DIR
-      #     mkdir $DIR
-      #     }}
-      # '';
+      q = ''quit'';
+      # zoxide integration
+      z = # bash
+        ''
+          %{{
+              result="$(zoxide query --exclude "$PWD" "$@" | sed 's/\\/\\\\/g;s/"/\\"/g')"
+              lf -remote "send $id cd \"$result\""
+          }}
+        '';
+      on-cd = # bash
+        ''
+          &{{
+            zoxide add "$PWD"
+          }}
+        '';
     };
     keybindings = {
       e = "open-nvim";
@@ -35,15 +42,37 @@
       "<backspace2>" = "set hidden!";
       "<enter>" = "open";
       f = "search";
-      F = "filter"; 
+      F = "filter";
     };
     cmdKeybindings = {
       "<tab>" = "cmd-menu-complete";
       "<backtab>" = "cmd-menu-complete-back";
     };
-    previewer = {
-      keybinding = "i";
-      source = null;
-    };
+    extraConfig =
+      let
+        previewer = pkgs.writeShellScriptBin "pv.sh" #bash
+        ''
+          file=$1
+          w=$2
+          h=$3
+          x=$4
+          y=$5
+
+          if [[ "$( ${pkgs.file}/bin/file -Lb --mime-type "$file")" =~ ^image ]]; then
+              ${pkgs.kitty}/bin/kitty +kitten icat --silent --stdin no --transfer-mode file --place "''${w}x''${h}@''${x}x''${y}" "$file" < /dev/null > /dev/tty
+              exit 1
+          fi
+
+          ${pkgs.pistol}/bin/pistol "$file"
+        '';
+        cleaner = pkgs.writeShellScriptBin "clean.sh" #bash
+        ''
+          ${pkgs.kitty}/bin/kitty +kitten icat --clear --stdin no --silent --transfer-mode file < /dev/null > /dev/tty
+        '';
+      in
+      ''
+        set cleaner ${cleaner}/bin/clean.sh
+        set previewer ${previewer}/bin/pv.sh
+      '';
   };
 }
