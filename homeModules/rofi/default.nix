9bf0e17847ebab5ad7983dec36a87f0856775b78
Ladas552 <l.tokshalov@gmail.com>
2025-03-06 13:20:24 +0500

    update

--- homeModules/rofi/default.nix
+++ homeModules/rofi/default.nix
@@ -17,7 +17,7 @@
       package = pkgs.rofi-wayland;
       font = "JetBrains Mono Nerd Font 11";
       # terminal = "${lib.getExe inputs.ghostty.packages.x86_64-linux.default}";
-      terminal = "${lib.getExe pkgs.ghostty}";
+      terminal = "${lib.getExe' pkgs.ghostty "ghostty"}";
       theme = ./theme.rasi;
     };
   };
