6f97abe8c0406718c539da801a06d3ef93d1cf83
Ladas552 <l.tokshalov@gmail.com>
2025-03-12 15:55:43 +0500

    I gave up

--- homeModules/imv/default.nix
+++ homeModules/imv/default.nix
@@ -9,6 +9,7 @@
     programs.imv = {
       enable = true;
     };
+    # it isn't using HM module settings because I don't know how to make them work. Like actually, imv.settings just outputs a file that binds for imv don't read, idk why
     home.file.".config/imv/config" = {
       source = ./config;
     };
