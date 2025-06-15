924d19c32677ad12eac7dfcedaf8c3d3ca3b0fca
Ladas552 <l.tokshalov@gmail.com>
2025-06-16 02:19:08 +0500

    some comments

--- pkgs/addcommitpush.nix
+++ pkgs/addcommitpush.nix
@@ -2,8 +2,9 @@
 let
   git = lib.getExe' pkgs.git "git";
 in
-# Adds `ass` subtitle files to mkv files and creates subdirectory for copied mkv files. Subtitles aren't burned into mkv, just added to the pool. Nice if you don't have subtitle fuzzy find in your Media Player
+# Quick git alias
+# gcp "your commit" to push new commit
 pkgs.writeShellScriptBin "gcp" # bash
   ''
-    ${git} add --all; ${git} commit -m "$1"; ${git} push
+    ${git} add --all && ${git} commit -m "$1" && ${git} push
   ''
