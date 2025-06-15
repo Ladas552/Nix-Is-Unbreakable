7f6ff1a230de95b8dca71e551ceb41673f4cf9e3
Ladas552 <l.tokshalov@gmail.com>
2025-06-16 02:33:55 +0500

    no redundant scripts folder

--- scripts/wpick.nix
+++
@@ -1,6 +0,0 @@
-{ pkgs, lib }:
-# It is a one liner to replace xpick on Wayland. Compatible with Wlroots. Doesn't need dependencies outside of ImageMagick that already declared in host
-pkgs.writeShellScriptBin "wpick" # bash
-  ''
-    ${lib.meta.getExe' pkgs.grim "grim"} -g "$(${lib.meta.getExe' pkgs.slurp "slurp"} -p)" -t ppm - | ${lib.meta.getExe' pkgs.imagemagick "magick"} - -format '%[pixel:p{0,0}]' txt:-
-  ''
