f108f4a6cc214edf934b9549983262cc18548c3d
Ladas552 <l.tokshalov@gmail.com>
2025-07-12 03:44:12 +0500

    zmedia for nixtoks

--- hosts/NixToks/default.nix
+++ hosts/NixToks/default.nix
@@ -166,4 +166,11 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.11"; # Did you read the comment?
+
+  ##### ZFS MOUNT POINTS
+  ##### Because I have additional drive for NixToks
+  fileSystems."/mnt/zmedia" = {
+    device = "zmedia/files";
+    fsType = "zfs";
+  };
 }
