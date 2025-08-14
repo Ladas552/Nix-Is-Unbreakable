a341e95e5ec0a889a706369bb8fd308fff4377f0
Ladas552 <l.tokshalov@gmail.com>
2025-08-14 10:07:56 +0500

    update, back to xanmod kernel

--- hosts/NixPort/default.nix
+++ hosts/NixPort/default.nix
@@ -65,8 +65,8 @@
     useGlobalPkgs = true;
   };
   # Xanmod kernel
-  # boot.kernelPackages = pkgs.linuxKernel.packages.linux_xanmod;
-  boot.kernelPackages = pkgs.linuxKernel.packages.linux_zen;
+  boot.kernelPackages = pkgs.linuxKernel.packages.linux_xanmod;
+  # boot.kernelPackages = pkgs.linuxKernel.packages.linux_zen;
 
 
   # https://wiki.archlinux.org/title/Lenovo_ThinkPad_T14s_(AMD)_Gen_3#Display
